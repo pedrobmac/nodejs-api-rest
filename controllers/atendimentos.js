f7039ab769b8a6733381fa36c0c1d0a19818f165
Pedro Bittencourt <pbittenc@gmail.com>
2021-10-16 20:28:09 -0300

    inclusao de metodo post em atendimentos

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,3 +1,7 @@
 module.exports = app => {
-    app.get("/atendimentos", (req, res) => res.send("Rota de Atendimentos atualizando o GET"))
+    app.get("/atendimentos", (req, res) => res.send("Rota de Atendimentos realizando um GET"))
+    
+    app.post("/atendimentos", (req, res) => {
+        console.log(req.body)
+        res.send("Rota de Atendimentos realizando um POST")})
 }
--- index.js
+++ index.js
@@ -1,12 +1,5 @@
-const express = require("express")
-const consign = require("consign")
+const customExpress = require("./config/customExpress")
 
-const app = express()
-
-consign()
-    .include("controllers")
-    .into(app)
+const app = customExpress()
 
 app.listen(3000, () => console.log("Servidor rodando em localhost:3000"))
-
-app.get("/", (req, res) => res.send("Servidor executando"))
